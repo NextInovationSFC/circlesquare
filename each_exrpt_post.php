b4040dd65105af7b5c30fbdc96e649a047b0b9a7
tatsumu <t.m.555.tennis@gmail.com>
2017-02-28 00:02:35 +0900

    initial commit

--- each_exrpt_post.php
+++ each_exrpt_post.php
@@ -4,9 +4,13 @@
     <a href="<?php the_permalink();?>" >
     <div class="wrap-col">
         <div class="random_icon" style="background: url('<?php echo $postImgSrc[0] ?> ') center center;    background-size: cover;"></div>
+        <div class="random_tags">
+        <?php
+            for ($i = 0; $i <= count($category); ++$i) {
+                echo $category[$i]->cat_name;
+        }?>
+        </div>
         <h2 class="random_name" style="letter-spacing: 3px;"><?php echo post_custom('name'); ?></h2>
-<!--        <div class="random_hr"></div>-->
-        <hr>
         <div class="random_description"><?php echo post_custom('short'); ?></div>
     </div>
     </a>
