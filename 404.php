9dfd4ba89bec6be063a1996eafb541fc4bb978c8
daifuku <fis10sai@i.softbank.jp>
2017-03-13 15:35:19 +0900

    add notfound.jpg and h2tag , p tag

--- 404.php
+++ 404.php
@@ -1,4 +1,12 @@
 <?php get_header(); ?>
 
+<div class="container">
+    <div class="card">
+    <h2 class="card__heading">404 not found</h2>
+    <p class="card__description">お探しのページは<br>ありませんでした</p>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/notfound.jpg" class = "card__image" alt="">
+    </div>
+</div>
+
 
 <?php get_footer(); ?>
