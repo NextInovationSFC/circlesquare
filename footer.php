5c58908fcd0c207d2aa1a68aaad80970864f90f3
daifuku <fis10sai@i.softbank.jp>
2017-03-15 03:04:21 +0900

    modify html file

--- footer.php
+++ footer.php
@@ -1,23 +1,32 @@
 <footer>
-     <div class="footer_nav">
-         <ul class="footer_nav-main">
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">HOME</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">CONTACT</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>about">このサイトについて</a></li>
-         </ul>
-         <ul class="footer_nav-policy">
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>sitepolicy">サイトポリシー</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>privacypolicy">プライバシーポリシー</a></li>
-         </ul>
-     </div>
-     <div class="clear"></div>
-     <p class="copyright">Copyright © NEXT INNOVATION inc. All Rights Reserved.</p>
- </footer>
- <!--footer_END-->
+    <div class="footer_nav">
+        <ul class="footer_nav-main">
+            <li>
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">HOME</a></li>
+            <li>
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">CONTACT</a>
+            </li>
+            <li>
+                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>about">About</a>
+            </li>
+        </ul>
+        <ul class="footer_nav-policy">
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>sitepolicy">サイトポリシー</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>privacypolicy">プライバシーポリシー</a></li>
+        </ul>
+    </div>
+    <div class="clear"></div>
+    <p class="copyright">Copyright © NEXT INNOVATION</p>
+</footer>
 
 
 
-  <?php wp_footer(); ?>
+
+<?php wp_footer(); ?>
 
 </body>
+
 </html>
