9b8ea30cd8ae2ffe99f346e0db8e5cd9b3a9faf0
kirimy <tonarinototoro7@icloud.com>
2017-03-15 15:19:10 +0900

    フッター修正版

--- footer.php
+++ footer.php
@@ -1,9 +1,19 @@
 <footer>
      <div class="footer_nav">
+
+        <div class="footer_nav-title">
+          <a class="footer_nav-title-cirlce" href="">Circle Square</a>
+          <a class="footer_nav-title-email">Email : circlesquare.sfc@gmail.com</a>
+         </div>
+
+
          <ul class="footer_nav-main">
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">HOME</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">CONTACT</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>about">このサイトについて</a></li>
+             <li><i class="fa fa-home fa-2x" aria-hidden="true"></i>
+               <a href="<?php echo esc_url( home_url( '/' ) ); ?>">HOME</a></li>
+             <li><i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+               <a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">CONTACT</a></li>
+             <li><i class="fa fa-info-circle fa-2x" aria-hidden="true"></i>
+               <a href="<?php echo esc_url( home_url( '/' ) ); ?>about">このサイトについて</a></li>
          </ul>
          <ul class="footer_nav-policy">
              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>sitepolicy">サイトポリシー</a></li>
@@ -11,7 +21,7 @@
          </ul>
      </div>
      <div class="clear"></div>
-     <p class="copyright">Copyright © NEXT INNOVATION inc. All Rights Reserved.</p>
+     <p class="copyright">Copyright © 2017 NEXT INNOVATION</p>
  </footer>
  <!--footer_END-->
 
