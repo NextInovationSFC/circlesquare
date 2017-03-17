ab49d53029dff28e97cd289debb765d086f24984
daifuku <fis10sai@i.softbank.jp>
2017-03-18 00:50:07 +0900

    modify footer css

--- footer.php
+++ footer.php
@@ -1,33 +1,37 @@
 
-
 <footer>
-     <div class="footer_nav">
+    <div class="footer__nav">
+        <ul class="footer__nav__main">
+            <li>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Circle Square</a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <i class="fa fa-home fa-fw" aria-hidden="true"></i>  Home</a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">
+                    <i class="fa fa-envelope fa-fw" aria-hidden="true"></i>  Contact</a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>about">
+                    <i class="fa fa-exclamation-circle fa-fw" aria-hidden="true"></i>  About</a>
+            </li>
+        </ul>
 
-        <div class="footer_nav-title">
-          <a class="footer_nav-title-cirlce" href="">Circle Square</a>
-          <a class="footer_nav-title-email">Email : circlesquare.sfc@gmail.com</a>
-         </div>
+        <ul class="footer__nav__policy">
+            <li>
+                <a href="mailto:circlesquare@gmail.com">Email : circlesquare@gmail.com</a>
+            </li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>sitepolicy">サイトポリシー</a></li>
+            <div class = "verticalLine" style="background-color:#bdbdbd; width:2px; height:28px;  border-radius: 1px;"></div>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>privacypolicy">プライバシーポリシー</a></li>
+        </ul>
+    </div>
 
-
-         <ul class="footer_nav-main">
-             <li><i class="fa fa-home fa-2x" aria-hidden="true"></i>
-               <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-             <li><i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
-               <a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">Contact</a></li>
-             <li><i class="fa fa-info-circle fa-2x" aria-hidden="true"></i>
-               <a href="<?php echo esc_url( home_url( '/' ) ); ?>about">About</a></li>
-         </ul>
-         <ul class="footer_nav-policy">
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>sitepolicy">サイトポリシー</a></li>
-             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>privacypolicy">プライバシーポリシー</a></li>
-         </ul>
-     </div>
-     <div class="clear"></div>
-     <p class="copyright">Copyright © 2017 NEXT INNOVATION</p>
- </footer>
- <!--footer_END-->
-
-
+    <div class="clear"></div>
+    <p class="copyright">Copyright © Next Inovation</p>
+</footer>
 
 
 
