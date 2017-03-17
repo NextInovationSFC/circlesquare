9d31802b80f2def0194e9f1d6178f6951fc09595
daifuku <fis10sai@i.softbank.jp>
2017-03-18 03:21:53 +0900

    画像以外は出来た　モバイルもデスクトップも

--- page-about.php
+++ page-about.php
@@ -1,89 +1,100 @@
 <?php get_header(); ?>
 
-<section class="about">
 
-  <div class="about_head">
-    <div class="about_title">
-      <div class="about_table">
-        <h2 class="head_copy">君とサークルの交差点</h2>
-        <h2 class="head_title">Circle Square</h2>
-      </div>
+    <div class="imageBox">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/home_img.jpg" alt="">
+        <!--
+    <h2 class="head_copy">君とサークルの交差点</h2>
+    <h2 class="head_title">Circle Square</h2>
+-->
     </div>
-  </div>
 
-  <div class="about_sentence">
-      <div class="screen-reader-response"></div>
-      <h3>About</h3>
 
-      <p>Circle Squareは、主に慶應義塾大学SFCの在学生や、これからSFCに入学する新入生の方へ向けた、サークル情報掲載サイトです。 </p>
-      <br>
-      <p>サイトの特徴として、サークルごとにカテゴリーが設けられており、各サークルのページにはそれぞれの活動日、年間の活動内容や連絡先が掲載されています。</p>
-      <p>現在、カテゴリー検索、キーワード検索が可能となっています。</p>
-      <p>サークル情報は、SFC新歓実行委員会様のご協力をいただいております。</p>
-      <p>サークル情報の修正、追加などに関しましては、コンタクトページまたは、circlesquare.sfc@gmail.comまでご連絡ください。</p>
-      <br>
-      <p>また当サイトご利用につきましては、サイトポリシーをご覧ください。</p>
-  </div>
+    <section class="about">
 
-  <div class="about_member">
-      <h3>Member</h3>
-      <div class="about_border">
-          <span class="b_red"></span>
-          <span class="b_blue"></span>
-          <span class="b_yellow"></span>
-          <span class="b_green"></span>
-      </div>
-    <div class="about_member-card">
 
-      <div class="about_member-tatsumu member">
+
+        <div class="about_sentence">
+            <div class="screen-reader-response"></div>
+            <h3 class="about_heading">About</h3>
+            <div class="lineBox">
+                <hr class="heading_line blue">
+                <hr class="heading_line yellow">
+                <hr class="heading_line red">
+                <hr class="heading_line green">
+            </div>
+            <p class="about_description">Circle Squareは、主に慶應義塾大学SFCの在学生や、これからSFCに入学する新入生の方へ向けた、サークル情報掲載サイトです。サイトの特徴として、サークルごとにカテゴリーが設けられており、各サークルのページにはそれぞれの活動日、年間の活動内容や連絡先が掲載されています。現在、カテゴリー検索、キーワード検索が可能となっています。サークル情報は、SFC新歓実行委員会様のご協力をいただいております。サークル情報の修正、追加などに関しましては、コンタクトページまたは、circlesquare.sfc@gmail.comまでご連絡ください。また当サイトご利用につきましては、サイトポリシーをご覧ください。</p>
+        </div>
+
+
+        <div class="about_member">
+            <h3 class="member_heading">Member</h3>
+            <div class="lineBox">
+                <hr class="heading_line blue">
+                <hr class="heading_line yellow">
+                <hr class="heading_line red">
+                <hr class="heading_line green">
+            </div>
+            <div class="about_member-card">
+
+                <a class="about_member-daifuku member" href="http://localhost:8888/2017/03/19/">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/member_daifuku.jpg" alt="" class="member_photo">
+            <p class="name">藤木 良祐</p>
+            <p class="about-circle">S.A.L.</p>
+            <hr class="line yellow">
+            <p class="about-position">Engineer Leader</p>
+
+        </a>
+
+                <a class="about_member-tatsumu member" href="http://localhost:8888/2017/03/19/">
           <img src="<?php echo get_template_directory_uri(); ?>/img/member_tatsumu.jpg" alt="" class="member_photo">
         <p class="name">松井 達夢</p>
-        <p class="about-circle red">NaDaRe</p>
-        <p class="about-position">Project manager</p>
+        <p class="about-circle">NaDaRe</p>
+            <hr class="line red">
+        <p class="about-position">Project Manager</p>
         <p> </p>
-      </div>
+      </a>
 
-      <div class="about_member-daifuku member">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/member_daifuku.jpg" alt="" class="member_photo">
-        <p class="name">藤木 良祐</p>
-        <p class="about-circle yellow">S.A.L.</p>
-        <p class="about-position">Engineer leader</p>
 
-      </div>
 
-      <div class="about_member-yuyu member">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/member_yuyu.jpg" alt="" class="member_photo">
-        <p class="name">原 有璃</p>
-        <p class="about-circle red">TopDogs</p>
-        <p class="about-position">Engineer</p>
-        <p> </p>
-      </div>
+                <a class="about_member-a-chan member" href="http://localhost:8888/2017/03/19/">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/member_a-chan.jpg" alt="" class="member_photo">
+            <p class="name">村松 明日香</p>
+            <p class="about-circle">Keio Collection</p>
+            <hr class="line blue">
+            <p class="about-position">Designer Leader</p>
+        </a>
 
-      <div class="about_member-honami member">
+
+                <a class="about_member-honami member" href="http://localhost:8888/2017/03/19/">
           <img src="<?php echo get_template_directory_uri(); ?>/img/member_honami.jpg" alt="" class="member_photo">
         <p class="name">久保 帆奈美</p>
-        <p class="about-circle red">TopDogs</p>
+        <p class="about-circle">TopDogs</p>
+            <hr class="line red">
         <p class="about-position">Designer</p>
-      </div>
+      </a>
 
-      <div class="about_member-a-chan member">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/member_a-chan.jpg" alt="" class="member_photo">
-        <p class="name">村松 明日香</p>
-        <p class="about-circle blue">Keio Collection</p>
-        <p class="about-position">Designer leader</p>
-      </div>
-
-      <div class="about_member-kirimi member">
+                <a class="about_member-kirimi member" href="http://localhost:8888/2017/03/19/">
           <img src="<?php echo get_template_directory_uri(); ?>/img/member_kirimi.jpg" alt="" class="member_photo">
         <p class="name">沼倉 朋哉</p>
-        <p class="about-circle green">ACOGGYZ</p>
+        <p class="about-circle">ACOGGYZ</p>
+            <hr class="line green">
         <p class="about-position">Engineer</p>
-      </div>
+      </a>
 
-    </div>
+                <a class="about_member-yuyu member" href="http://localhost:8888/2017/03/19/">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/member_yuyu.jpg" alt="" class="member_photo">
+            <p class="name">原 有璃</p>
+            <p class="about-circle">TopDogs</p>
+            <hr class="line red">
+            <p class="about-position">Engineer</p>
+            <p> </p>
+        </a>
 
-  </div>
+            </div>
 
- </section>
+        </div>
 
- <?php get_footer(); ?>
+    </section>
+
+    <?php get_footer(); ?>
